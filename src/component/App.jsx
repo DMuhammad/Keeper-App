f3aeff0d8ca34f5d3dc50af65c6b832afe4a0dbc
Dzikri Muhammad <notfnd123@gmail.com>
2022-04-02 17:37:14 +0700

    Part 3 Completed

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
+import CreateArea from "./CreateArea";
 import Footer from "./Footer";
-import notes from "../notes";
+import {v4 as uuidv4} from "uuid";
 
 function App(){
+    const [listNote, setListNote] = useState([]);
+
+    function addNoteItem(note){
+        setListNote((prevNote) => {
+            return [...prevNote, note];
+        })
+    }
+
+    function deleteNoteItem(id){
+        setListNote((prevNote) => {
+            return prevNote.filter((note, index) => {
+                return index !== id
+            })
+        })
+    }
+
     return (
         <div>
             <Header></Header>
-            {notes.map(note => <Note key={note.key} title={note.title} content={note.content} />)}
+            <CreateArea onAdd={addNoteItem} />
+            {listNote.map((item, index) => {
+                return <Note key={uuidv4()} id={index} title={item.title} content={item.content} onDel={deleteNoteItem} />
+            })}
             <Footer></Footer>
         </div>
     )
